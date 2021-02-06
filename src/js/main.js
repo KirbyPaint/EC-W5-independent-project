ae811f09e1c5a7de04401dff423d497959613679
Ashley Porter <porter.ashley13@gmail.com>
2021-02-05 16:02:48 -0800

    add live calculation of life expectancy to site

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,20 @@ import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/styles.css';
-// import Supergalactic from './supergalactic.js';
+import Supergalactic from './supergalactic.js';
 
 $(document).on("click", ":submit", function(event){
   event.preventDefault();
-
+  
   const age = $("#userInputAge").val();
-  // const x = new Supergalactic(age);
-  // alert($(this).val());
-  alert(age);
+  const supergalactic = new Supergalactic(age);
+  const expectancy = $("#userInputExpectancy").val();
+  const planet = $(this).val();
+
+  $(".age").val(age);
+  $(".planet").val(planet);
+  $(".expectancy").val(expectancy);
+  
+  console.log(supergalactic.age);
+  console.log(supergalactic.lifeExpectancy(age, expectancy, planet));
 });
