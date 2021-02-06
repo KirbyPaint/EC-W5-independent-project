228c6132e47ca7c6368f7dca6b131661ec9413cf
Ashley Porter <porter.ashley13@gmail.com>
2021-02-05 21:50:34 -0800

    correct tests for life expectancy for all planets

--- __tests__/supergalactic.jest.js
+++ __tests__/supergalactic.jest.js
@@ -32,28 +32,28 @@ describe('Supergalactic', () => {
     const age = 25;
     const expectancy = 80;
     const planet = "Mercury"
-    expect(supergalactic.lifeExpectancy(age, expectancy, planet)).toEqual(229);
+    expect(supergalactic.lifeExpectancy(age, expectancy, planet)).toEqual(229.16);
   });
 
   test('should return the estimated years left to live on Venus based on user\'s age (example as 25), given demographic expectancy (example of 80)', () => {
     const age = 25;
     const expectancy = 80;
     const planet = "Venus"
-    expect(supergalactic.lifeExpectancy(age, expectancy, planet)).toEqual(90);
+    expect(supergalactic.lifeExpectancy(age, expectancy, planet)).toEqual(88.71);
   });
 
   test('should return the estimated years left to live on Mars based on user\'s age (example as 25), given demographic expectancy (example of 80)', () => {
     const age = 25;
     const expectancy = 80;
     const planet = "Mars"
-    expect(supergalactic.lifeExpectancy(age, expectancy, planet)).toEqual(29);
+    expect(supergalactic.lifeExpectancy(age, expectancy, planet)).toEqual(29.25);
   });
 
   test('should return the estimated years left to live on Jupiter based on user\'s age (example as 25), given demographic expectancy (example of 80)', () => {
     const age = 25;
     const expectancy = 80;
     const planet = "Jupiter"
-    expect(supergalactic.lifeExpectancy(age, expectancy, planet)).toEqual(4);
+    expect(supergalactic.lifeExpectancy(age, expectancy, planet)).toEqual(4.64);
   });
 
   test('should return the number of years the user has lived past the life expectancy', () => {
